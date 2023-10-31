9ada35a13a347b3843ef8273be219c94943b6ba8
Kev <87592672+0xkev21@users.noreply.github.com>
2023-10-31 22:50:58 +0630

    add user input feature for custom pixel count
    The user can now choose the pixel count from 6 to 100 as will.

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 const container = document.querySelector('.container');
-const pixelCount = 16 * 16;
+const column = document.querySelector('#col-count'); // input for column count
 const color = 'black';
 
-// Create pixels inside the container to make sketchpad
-function createSketch(pCount) {
+// function to create pixels inside the container to make sketchpad
+function createSketch() {
+
+    // clear the container
+    container.innerHTML = '';
+
+    const colCount = column.value;
+    const pixelCount = colCount**2; // square of col count
+
+    // create dom fragment for better performance
     const fragment = new DocumentFragment();
-    for (let i = 0; i < pCount; i++) {
+    
+    for (let i = 0; i < pixelCount; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        
+        // reset the width of each pixel
+        pixel.style.setProperty('--pixel-count', colCount);
+
         fragment.appendChild(pixel);
     }
     container.appendChild(fragment);
 }
-createSketch(pixelCount);
+createSketch();
 
 // initiate painting with a boolean value
 let painting = false;
-
 function draw(e) {
     if (!painting) return;
     e.target.classList.add('draw');
 }
-
 // start the painting on mousedown
 document.body.addEventListener('mousedown', function(e) {
     painting = true;
     draw(e);
 });
-
-// continue the draw on mousemove and mouseenter the container
-container.addEventListener('mouseenter', function(e) {
-    draw();
-});
+// continue the drawing during mousemove
 container.addEventListener('mousemove', draw);
-
 // end the painting on mouseup
 document.body.addEventListener('mouseup', () => {painting = false;});
+
+column.addEventListener('change', createSketch);
+
+//display the pixels count in the DOM
+column.addEventListener('input', () => {
+    const displayPixels = document.querySelectorAll('.pixels');
+    displayPixels.forEach(p => {
+        p.textContent = column.value;
+    })
+})
